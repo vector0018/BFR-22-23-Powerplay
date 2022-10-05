dfecdf0b4950ac37d9497306b94f62df90fd5a5b
Samikri <Samikrichene10@gmail.com>
2022-10-04 20:31:24 -0400

    Added claw code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleoptests/Slide_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleoptests/Slide_test.java
@@ -11,14 +11,25 @@ public class Slide_test extends LinearOpMode {
         Servo leftClaw = null;
         Servo rightClaw = null;
         double slidePower = gamepad2.left_stick_y * 0.7;
-        slideMotor = hardwareMap.get(DcMotor.class,"SM");
-        waitForStart();
 
-        while (opModeIsActive()) {
-            slideMotor.setPower(slidePower);
-            if (gamepad2.left_bumper );
+        slideMotor = hardwareMap.get(DcMotor.class, "SM");
+        leftClaw = hardwareMap.get(Servo.class, "LC");
+        rightClaw = hardwareMap.get(Servo.class, "RC");
 
-            if (gamepad2.right_bumper);
-        }
+
+
+            waitForStart();
+
+            while (opModeIsActive()) {
+                slideMotor.setPower(slidePower);
+
+                if (gamepad2.left_bumper){
+                    leftClaw.setPosition(0);
+                }
+                if (gamepad2.right_bumper){
+                    leftClaw.setPosition(0);
+            }
+
     }
+}
 }
