3c9659a47723f52932adc9677f413ab64bdc5e83
vector0018 <92063929+vector0018@users.noreply.github.com>
2022-10-08 18:54:44 -0400

    Ran slide test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleoptests/SlideConstants.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.teleop.teleoptests;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+@Config
+public class SlideConstants {
+    public static final double pulleyCircumfirence = 4.40945;
+    public static final double slideTicksPerRev = 537.7;
+    public static final double maxRPM = 312;
+
+    public static final double maxTargetPosition = 42;
+    public static final double minTargetPosition = 0;
+
+    public static final boolean RUN_USING_ENCODER = true;
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0.1, 0, 0,
+            getMotorVelocityF(maxRPM / 60 * slideTicksPerRev));
+
+    public static double kV = 1.0 / rpmToVelocity(maxRPM);
+    public static double kA = 0;
+    public static double kStatic = 0;
+
+    public static double maxVel = 50;
+    public static double maxAccel = 30;
+
+    public static double encoderTicksToInches(double ticks) {
+        return pulleyCircumfirence * ticks / slideTicksPerRev;
+    }
+
+    public static double rpmToVelocity(double rpm) {
+        return rpm * 2 * Math.PI * pulleyCircumfirence / 60.0;
+    }
+
+    public static double getMotorVelocityF(double ticksPerSecond) {
+        // see https://docs.google.com/document/d/1tyWrXDfMidwYyP_5H4mZyVgaEswhOC35gvdmP-V-5hA/edit#heading=h.61g9ixenznbx
+        return 32767 / ticksPerSecond;
+    }
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleoptests/Slide_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleoptests/Slide_test.java
@@ -1,9 +1,16 @@
 package org.firstinspires.ftc.teamcode.teleop.teleoptests;
 
+import static org.firstinspires.ftc.teamcode.teleop.teleoptests.SlideConstants.encoderTicksToInches;
+import static org.firstinspires.ftc.teamcode.teleop.teleoptests.SlideConstants.maxTargetPosition;
+import static org.firstinspires.ftc.teamcode.teleop.teleoptests.SlideConstants.minTargetPosition;
+import static org.firstinspires.ftc.teamcode.teleop.teleoptests.SlideConstants.slideTicksPerRev;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+
 @TeleOp
 public class Slide_test extends LinearOpMode {
 
@@ -11,22 +18,58 @@ public class Slide_test extends LinearOpMode {
         DcMotor slideMotor = null;
         Servo leftClaw = null;
         Servo rightClaw = null;
+        double Kp = 0.1;
+        double currentPosition;
+        double targetPosition = 0;
+        double positionError;
+        double slidePower;
+        double zeroPos = 0;
+
 
         slideMotor = hardwareMap.get(DcMotor.class, "SM");
         leftClaw = hardwareMap.get(Servo.class, "LC");
         rightClaw = hardwareMap.get(Servo.class, "RC");
+        slideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slideMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        zeroPos = encoderTicksToInches(slideMotor.getCurrentPosition());
 
             waitForStart();
 
             while (opModeIsActive()) {
-                double slidePower = gamepad2.left_stick_y * 0.7;
+                targetPosition += gamepad2.left_stick_y * -0.2;
+                if (targetPosition > maxTargetPosition) {
+                    targetPosition = maxTargetPosition;
+                }
+                if (targetPosition < minTargetPosition) {
+                    targetPosition = minTargetPosition;
+                }
+                currentPosition = encoderTicksToInches(slideMotor.getCurrentPosition()) - zeroPos;
+                positionError = targetPosition - currentPosition;
+                slidePower = Kp * positionError;
+                telemetry.addData("target position: ", targetPosition);
+                telemetry.addData("position error: " , positionError);
+                telemetry.addData("current position", currentPosition);
+                telemetry.addData("slide power: " , slidePower);
+                telemetry.addData("Zero Position: ", zeroPos);
+                telemetry.addData("Right servo: ", rightClaw.getPosition());
+                telemetry.addData("Left servo: ", leftClaw.getPosition());
+                telemetry.update();
+
+                if (slidePower > 1) {
+                    slidePower = 1;
+                }
+                if (slidePower < -1) {
+                    slidePower = -1;
+                }
                 slideMotor.setPower(slidePower);
 
                 if (gamepad2.left_bumper){
-                    leftClaw.setPosition(.5);
+                    leftClaw.setPosition(-.5);
+                    rightClaw.setPosition(.5);
                 }
                 if (gamepad2.right_bumper){
-                    rightClaw.setPosition(.5);
+                    leftClaw.setPosition(-1);
+                    rightClaw.setPosition(1);
             }
 
     }
