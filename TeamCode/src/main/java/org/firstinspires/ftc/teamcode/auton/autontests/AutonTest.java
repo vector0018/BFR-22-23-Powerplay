186f13d528b2bd610e8f73546c2093c396c8efeb
Samikri <Samikrichene10@gmail.com>
2022-10-17 19:37:32 -0400

    Began auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/autontests/AutonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/autontests/AutonTest.java
@@ -1,5 +1,5 @@
 
-package org.firstinspires.ftc.teamcode.teleop.teleoptests;
+package org.firstinspires.ftc.teamcode.auton.autontests;
 
 import static org.firstinspires.ftc.teamcode.teleop.teleoptests.SlideConstants.encoderTicksToInches;
 import static org.firstinspires.ftc.teamcode.teleop.teleoptests.SlideConstants.maxTargetPosition;
@@ -8,6 +8,7 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import static org.firstinspires.ftc.teamcode.teleop.teleoptests.SlideConstants.slideTicksPerRev;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -15,8 +16,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
-@TeleOp (name = "Field Centric")
-public class fieldSentricTeleop extends LinearOpMode {
+@Autonomous(name =  "Blue 1" )
+public class AutonTest extends LinearOpMode {
 
     public void runOpMode() throws InterruptedException {
 
@@ -24,11 +25,8 @@ public class fieldSentricTeleop extends LinearOpMode {
         DcMotor slideMotor;
         Servo leftClaw;
         Servo rightClaw;
-        double Kp = 0.2;
         double currentPosition;
         double targetPosition = 0;
-        double positionError;
-        double slidePower;
         double zeroPos;
         double xVal;
         double yVal;
@@ -48,33 +46,22 @@ public class fieldSentricTeleop extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-            xVal = -gamepad1.left_stick_y;
-            yVal = -gamepad1.left_stick_x;
-            angleVal = -gamepad1.right_stick_x;
+            rightClaw.setPosition(0.5);
+            leftClaw.setPosition(0.6);
 
-            if (gamepad1.left_trigger > .1 || gamepad1.right_trigger > .1){
-                xVal *= 0.3;
-                yVal *= 0.3;
-                angleVal *= 0.3;
-            }
-
-            Vector2d input = new Vector2d(
-                xVal,
-                yVal)
-                .rotated(-drive.getRawExternalHeading());
-
-                drive.setWeightedDrivePower(
-                new Pose2d(
-                        input.getX(),
-                        input.getY(),
-                         angleVal
-                )
-        );
+            currentPosition = encoderTicksToInches(slideMotor.getCurrentPosition()) - zeroPos;
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            .getX(),
+                            input.getY(),
+                            angleVal
+                    )
+            );
             // add 0.2 inches to target position when left stick is pushed up or down
             if (Math.abs(gamepad2.left_stick_y) > .01) {
                 targetPosition += gamepad2.left_stick_y * -0.4;
             }
-             else if (gamepad2.a){
+            else if (gamepad2.a){
                 targetPosition = 2;
             }
             //low junction
@@ -90,37 +77,6 @@ public class fieldSentricTeleop extends LinearOpMode {
                 targetPosition = 35.5;
             }
 
-            //safeties for slide pos
-            if (targetPosition > maxTargetPosition) {
-                targetPosition = maxTargetPosition;
-            }
-            if (targetPosition < minTargetPosition) {
-                targetPosition = minTargetPosition;
-            }
-
-            //takes starting position and subtracts from current input to get real position
-            currentPosition = encoderTicksToInches(slideMotor.getCurrentPosition()) - zeroPos;
-            positionError = targetPosition - currentPosition;
-
-            //proportional pid controller
-            slidePower = Kp * positionError;
-
-            telemetry.addData("target position: ", targetPosition);
-            telemetry.addData("position error: ", positionError);
-            telemetry.addData("current position", currentPosition);
-            telemetry.addData("slide power: ", slidePower);
-            telemetry.addData("Zero Position: ", zeroPos);
-            telemetry.addData("Right servo: ", rightClaw.getPosition());
-            telemetry.addData("Left servo: ", leftClaw.getPosition());
-            telemetry.update();
-
-            //power safeties
-            if (slidePower > 1) {
-                slidePower = 1;
-            }
-            if (slidePower < -1) {
-                slidePower = -1;
-            }
             slideMotor.setPower(slidePower);
 
             //open claw
@@ -141,31 +97,34 @@ public class fieldSentricTeleop extends LinearOpMode {
                 leftClaw.setPosition(1);
                 rightClaw.setPosition(1);
             }
-
-            //binds for specific heights - don't touch left stick, added 2 inches to junction height
-
-
-            //ground junction
-            /*
-            if (gamepad2.a){
-                targetPosition = 2;
-                slideMotor.setPower(slidePower);
-            }
-            //low junction
-            if (gamepad2.b){
-                targetPosition = 15.5;
-                slideMotor.setPower(slidePower);
-            }
-            //medium junction
-            if (gamepad2.x) {
-                targetPosition = 25.45;
-                slideMotor.setPower(slidePower);
-            }
-            //high junction
-            if (gamepad2.y){
-                targetPosition = 35.5;
-                slideMotor.setPower(slidePower);
-            } */
         }
+    }
+    private double moveSlide (double currentPosition , double targetPosition) {
+        double Kp = 0.2;
+        double positionError;
+        double slidePower;
+
+        //safeties for slide pos
+        if (targetPosition > maxTargetPosition) {
+            targetPosition = maxTargetPosition;
+        }
+        if (targetPosition < minTargetPosition) {
+            targetPosition = minTargetPosition;
+        }
+
+        //takes starting position and subtracts from current input to get real position
+        positionError = targetPosition - currentPosition;
+
+        //proportional pid controller
+        slidePower = Kp * positionError;
+
+        //power safeties
+        if (slidePower > 1) {
+            slidePower = 1;
+        }
+        if (slidePower < -1) {
+            slidePower = -1;
+        }
+        return slidePower;
     }
 }
