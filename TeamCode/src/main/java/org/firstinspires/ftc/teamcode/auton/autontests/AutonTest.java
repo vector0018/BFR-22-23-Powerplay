ed7fa33f8c37d284dccb84ef8e98e874d62bdde3
Samikri <Samikrichene10@gmail.com>
2022-10-22 10:28:09 -0400

    Started auton test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/autontests/AutonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/autontests/AutonTest.java
@@ -6,8 +6,11 @@ import static org.firstinspires.ftc.teamcode.teleop.teleoptests.SlideConstants.m
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -23,6 +26,7 @@ public class AutonTest extends LinearOpMode {
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         DcMotor slideMotor;
+        ColorSensor colorSensor;
         Servo leftClaw;
         Servo rightClaw;
         double currentPosition;
@@ -38,11 +42,21 @@ public class AutonTest extends LinearOpMode {
         slideMotor = hardwareMap.get(DcMotor.class, "SM");
         leftClaw = hardwareMap.get(Servo.class, "LC");
         rightClaw = hardwareMap.get(Servo.class, "RC");
+        colorSensor = hardwareMap.get(RevColorSensorV3.class, "CS");
         slideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         slideMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         zeroPos = encoderTicksToInches(slideMotor.getCurrentPosition());
         leftClaw.setDirection(Servo.Direction.FORWARD);
         rightClaw.setDirection(Servo.Direction.REVERSE);
+
+        Trajectory moveToSignal = drive.trajectoryBuilder(new Pose2d())
+                .forward(15)
+                .build();
+        Trajectory moveToG3 = drive.trajectoryBuilder(moveToSignal.end())
+                .strafeRight(3)
+                .build();
+
         waitForStart();
 
         rightClaw.setPosition(0.5);
@@ -50,12 +64,17 @@ public class AutonTest extends LinearOpMode {
         sleep(500);
 
         telemetry.addData("claw", 0);
+        telemetry.addData("Alpha: ", colorSensor.alpha());
+        telemetry.addData("Blue: ", colorSensor.blue());
+        telemetry.addData("Red: ", colorSensor.red());
+        telemetry.addData("Green: ", colorSensor.green());
+        telemetry.addData("argb; ", colorSensor.argb());
         telemetry.update();
 
         currentPosition = encoderTicksToInches(slideMotor.getCurrentPosition()) - zeroPos;
          runTime.reset();
-        while (currentPosition < 5.0 && runTime.seconds()<1) {
-            slidePower = moveSlide(currentPosition, 5);
+        while (currentPosition < 10.0 && runTime.seconds()<1.5) {
+            slidePower = moveSlide(currentPosition, 10);
             slideMotor.setPower(slidePower);
             telemetry.addData(
                     "Slide motor", slidePower);
@@ -65,55 +84,27 @@ public class AutonTest extends LinearOpMode {
 
         slideMotor.setPower(0);
 
-          drive.setWeightedDrivePower(new Pose2d(8, 0, 0));
-          sleep(500);
-          drive.setWeightedDrivePower(new Pose2d(-8, 0, 0));
-          sleep(500);
+        drive.followTrajectory(moveToSignal);
+        // TODO: color sensor code goes here
+        sleep(500);
+        drive.followTrajectory(moveToG3);
+        drive.turn(-Math.PI/2);
+        sleep(500);
+        currentPosition = encoderTicksToInches(slideMotor.getCurrentPosition()) - zeroPos;
+        runTime.reset();
+        while (currentPosition > 2.0 && runTime.seconds()<1.5) {
+            slidePower = moveSlide(currentPosition, 2);
+            slideMotor.setPower(slidePower);
+            telemetry.addData(
+                    "Slide motor", slidePower);
+            telemetry.update();
+            currentPosition = encoderTicksToInches(slideMotor.getCurrentPosition()) - zeroPos;
+        }
+
+        sleep(5000);
        // drive.setWeightedDrivePower(new Pose2d(2, 0, 45));
        // sleep(500);
 
-        /*
-        // add 0.2 inches to target position when left stick is pushed up or down
-        if (Math.abs(gamepad2.left_stick_y) > .01) {
-            targetPosition += gamepad2.left_stick_y * -0.4;
-        }
-        else if (gamepad2.a){
-            targetPosition = 2;
-        }
-        //low junction
-        else if (gamepad2.b){
-            targetPosition = 15.5;
-        }
-        //medium junction
-        else if (gamepad2.y) {
-            targetPosition = 25.45;
-        }
-        //high junction
-        else if (gamepad2.x){
-            targetPosition = 35.5;
-        }
-
-        slideMotor.setPower(slidePower);
-
-        //open claw
-        if (gamepad2.left_bumper) {
-            leftClaw.setPosition(.8);
-        }
-        if (gamepad2.left_bumper) {
-            rightClaw.setPosition(.7);
-        }
-        //close claw
-        if (gamepad2.right_bumper) {
-            rightClaw.setPosition(0.5);
-        }
-        if (gamepad2.right_bumper) {
-            leftClaw.setPosition(0.6);
-        }
-        if (gamepad2.left_trigger > .1 || gamepad2.right_trigger > .1){
-            leftClaw.setPosition(1);
-            rightClaw.setPosition(1);
-        }
-        */
     }
     private double moveSlide (double currentPosition , double targetPosition) {
         double Kp = 0.2;
