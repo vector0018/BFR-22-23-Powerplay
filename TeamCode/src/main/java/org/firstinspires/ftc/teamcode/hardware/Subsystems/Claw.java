d37dbbe5a12e41b8c80d09200e0661018272acdd
vector0018 <92063929+vector0018@users.noreply.github.com>
2022-10-29 11:18:44 -0400

    added methods for claw, verify

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Subsystems/Claw.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.hardware.Subsystems;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
+
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -20,5 +22,26 @@ public class Claw implements Subsystem{
     @Override
     public void sendTelemetry(Telemetry telemetry) {
 
+    }
+    public void moveClaw(){
+        //open claw
+        if (gamepad2.left_bumper) {
+            leftClaw.setPosition(.7);
+        }
+        if (gamepad2.left_bumper) {
+            rightClaw.setPosition(.7);
+        }
+        //close claw
+        if (gamepad2.right_bumper) {
+            rightClaw.setPosition(0.5);
+        }
+        if (gamepad2.right_bumper) {
+            leftClaw.setPosition(0.4);
+        }
+        // Open claw all; the way
+        if (gamepad2.left_trigger > .1 || gamepad2.right_trigger > .1){
+            leftClaw.setPosition(.9);
+            rightClaw.setPosition(.9);
+        }
     }
 }
