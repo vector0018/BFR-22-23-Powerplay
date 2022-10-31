763bffcb4150eb42863396a110b89b6ea0acacf1
vector0018 <92063929+vector0018@users.noreply.github.com>
2022-10-30 20:22:40 -0400

    Field Centric Teleop with Robot Hardware - I can spell

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Subsystems/Slide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Subsystems/Slide.java
@@ -37,37 +37,66 @@ public class Slide implements Subsystem {
 
     }
 
-    private void moveSlide(double targetPosition, double runTime) {
+    public void moveSlide(double targetPosition, double runTime) {
         double Kp = 0.2;
         double positionError;
         ElapsedTime timer = new ElapsedTime();
 
         currentPosition = encoderTicksToInches(slideMotor.getCurrentPosition()) - zeroPos;
         timer.reset();
-        while (Math.abs(currentPosition-targetPosition) < 0.15 && timer.seconds()<runTime) {
+        while (Math.abs(currentPosition - targetPosition) < 0.15 && timer.seconds() < runTime) {
 
-        //safeties for slide pos
-        if (targetPosition > maxTargetPosition) {
-            targetPosition = maxTargetPosition;
-        }
-        if (targetPosition < minTargetPosition) {
-            targetPosition = minTargetPosition;
-        }
-        //takes starting position and subtracts from current input to get real position
-        positionError = targetPosition - currentPosition;
+            //safeties for slide pos
+            if (targetPosition > maxTargetPosition) {
+                targetPosition = maxTargetPosition;
+            }
+            if (targetPosition < minTargetPosition) {
+                targetPosition = minTargetPosition;
+            }
+            //takes starting position and subtracts from current input to get real position
+            positionError = targetPosition - currentPosition;
 
-        //proportional pid controller
-        slidePower = Kp * positionError;
+            //proportional pid controller
+            slidePower = Kp * positionError;
 
-        //power safeties
-        if (slidePower > 1) {
-            slidePower = 1;
-        }
-        if (slidePower < -1) {
-            slidePower = -1;
-        }
+            //power safeties
+            if (slidePower > 1) {
+                slidePower = 1;
+            }
+            if (slidePower < -1) {
+                slidePower = -1;
+            }
             slideMotor.setPower(slidePower);
             currentPosition = encoderTicksToInches(slideMotor.getCurrentPosition()) - zeroPos;
         }
+    }
+
+        public void moveSlideWithoutTime(double targetPosition2) {
+            double Kp2 = 0.2;
+            double positionError2;
+
+            currentPosition = encoderTicksToInches(slideMotor.getCurrentPosition()) - zeroPos;
+
+            //safeties for slide pos
+            if (targetPosition2 > maxTargetPosition) {
+                targetPosition2 = maxTargetPosition;
+            }
+            if (targetPosition2 < minTargetPosition) {
+                targetPosition2 = minTargetPosition;
+            }
+            //takes starting position and subtracts from current input to get real position
+            positionError2 = targetPosition2 - currentPosition;
+
+            //proportional pid controller
+            slidePower = Kp2 * positionError2;
+
+            //power safeties
+            if (slidePower > 1) {
+                slidePower = 1;
+            }
+            if (slidePower < -1) {
+                slidePower = -1;
+            }
+            slideMotor.setPower(slidePower);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleoptests/FieldCentricTeleopRH.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode.teleop.teleoptests;
+
+import static org.firstinspires.ftc.teamcode.hardware.SlideConstants.encoderTicksToInches;
+import static org.firstinspires.ftc.teamcode.hardware.SlideConstants.maxTargetPosition;
+import static org.firstinspires.ftc.teamcode.hardware.SlideConstants.minTargetPosition;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import static org.firstinspires.ftc.teamcode.hardware.SlideConstants.slideTicksPerRev;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.robot.Robot;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
+import org.firstinspires.ftc.teamcode.hardware.Subsystems.Claw;
+import org.firstinspires.ftc.teamcode.hardware.Subsystems.Slide;
+
+@TeleOp (name = "Field Centric but I can spell")
+public class FieldCentricTeleopRH extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RobotHardware robot = RobotHardware.getInstance();
+        SampleMecanumDrive drive = robot.drive;
+        Slide slide = robot.slide;
+        Claw claw = robot.claw;
+
+        double xVal;
+        double yVal;
+        double angleVal;
+
+        double cntrlrTargetPosition = 0;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            xVal = -gamepad1.left_stick_y;
+            yVal = -gamepad1.left_stick_x;
+            angleVal = -gamepad1.right_stick_x;
+
+            if (gamepad1.left_trigger > .1 || gamepad1.right_trigger > .1){
+                xVal *= 0.3;
+                yVal *= 0.3;
+                angleVal *= 0.3;
+            }
+
+            Vector2d input = new Vector2d(
+                    xVal,
+                    yVal)
+                    .rotated(-drive.getRawExternalHeading());
+
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            input.getX(),
+                            input.getY(),
+                            angleVal
+                    )
+            );
+
+            if (Math.abs(gamepad2.left_stick_y) > .01) {
+                cntrlrTargetPosition += gamepad2.left_stick_y * -1;
+                robot.slide.moveSlideWithoutTime(cntrlrTargetPosition);
+            }
+            else if (gamepad2.a){
+                cntrlrTargetPosition = 2;
+                robot.slide.moveSlideWithoutTime(cntrlrTargetPosition);
+            }
+            //low junction
+            else if (gamepad2.b){
+                cntrlrTargetPosition = 15.5;
+                robot.slide.moveSlideWithoutTime(cntrlrTargetPosition);
+            }
+            //medium junction
+            else if (gamepad2.y) {
+                cntrlrTargetPosition = 25.45;
+                robot.slide.moveSlideWithoutTime(cntrlrTargetPosition);
+            }
+            //high junction
+            else if (gamepad2.x){
+                cntrlrTargetPosition = 35.5;
+                robot.slide.moveSlideWithoutTime(cntrlrTargetPosition);
+            }
+
+            robot.claw.moveClaw(gamepad2.right_bumper, gamepad2.left_bumper, gamepad1.left_trigger, gamepad2.right_trigger);
+        }
+    }
+}
